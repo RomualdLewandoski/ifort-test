53124cb51ca86d6b2a7cd4e821bd3626970297f0
Romuald Lewandoski <59136319+RomualdLewandoski@users.noreply.github.com>
2025-08-19 15:05:28 +0200

    Ajout toggle prioritaire (#7)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EleveController;
 use App\Http\Controllers\FormulaireController;
 use App\Http\Controllers\ListeController;
 use Illuminate\Support\Facades\Route;
@@ -7,3 +8,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ListeController::class, 'index'])->name('liste');
 Route::get('/ajouter', [FormulaireController::class, 'index'])->name('ajouter');
 Route::post('/ajouter', [FormulaireController::class,'store'])->name('ajouter.store');
+Route::prefix('eleve')->name('eleve.')->group(function () {
+   Route::patch('/star/{eleve}', [EleveController::class, 'toggleStar'])->name('star');
+});
